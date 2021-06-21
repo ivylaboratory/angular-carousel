1bc5619ed843d98a6c679232a6814da65ac56fe5
Nikita Drozhzhin <drozhzhin.n.e@mail.ru>
2021-06-21 19:15:47 +0300

    Extended set of examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,22 @@ import { Component, ViewChildren } from '@angular/core';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+	styleUrls: ['./app.component.sass']
 })
 
 export class AppComponent {
-	images = [
-		{path: '/assets/1.jfif'},
-		{path: '/assets/2.jpg'},
-		{path: '/assets/3.jpg'},
-		{path: '/assets/4.jfif'},
-		{path: '/assets/5.jpg'},
-		{path: '/assets/6.jpg'},
-		{path: '/assets/7.jpg'},
-		{path: '/assets/8.jpg'},
-		{path: '/assets/9.jpg'},
-		{path: '/assets/10.jfif'}
-	];
+    images = [
+        {path: '/assets/photo-1548625149-9129dad5eef7.jpg'},
+        {path: '/assets/photo-1548625149-d37da68f9a7f.jpg'},
+        {path: '/assets/photo-1489365091240-6a18fc761ec2.jpg'},
+        {path: '/assets/photo-1547691889-841a6f1c5ca6.jpg'},
+        {path: '/assets/photo-1595433562696-a8b1cb8bdad1.jpg'},
+        {path: '/assets/photo-1495563381401-ecfbcaaa60f2.jpg'},
+        {path: '/assets/photo-1534801022022-6e319a11f249.jpg'},
+        {path: '/assets/photo-1524324463413-57e3d8392df1.jpg'},
+        {path: '/assets/photo-1506086679524-493c64fdfaa6.jpg'},
+        {path: '/assets/photo-1569749450723-1836b067fb64.jpg'}
+    ];   
 
     imagesForSlider = [
         {path: '/assets/photo-1444065707204-12decac917e8.jfif'},
